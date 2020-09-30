80e827316c4a571bdb621c3b41d9039a3e057391
Lilian Burdianov <lilian.burdianov@gmail.com>
2020-09-30 11:56:49 +0400

    create trait singleton

--- functions.php
+++ functions.php
@@ -6,6 +6,12 @@
  * @package Aquila
  */
 
+if (!defined('AQUILA_DIR_PATH')) {
+  define('AQUILA_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+
+require_once AQUILA_DIR_PATH . '/inc/helpers/autoloader.php';
+
 function aquila_enqueue_scripts()
 {
   // Register styles
